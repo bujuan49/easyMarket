9b51f5447e3aa94186c11a62191f721b523af224
zwj <2317133489@qq.com>
2019-07-26 14:55:13 +0800

    首页

--- app/src/store/modules/home.js
+++ app/src/store/modules/home.js
@@ -4,11 +4,17 @@ import {home} from "../../services/index.js";
 export default class Home {
     @observable data=[];
     @observable List=[];
+    @observable dataList=[];
+    @observable product=[];
+    @observable hot=[];
     @action change(){
         home().then((res)=>{
-            console.log(res.data.data.channel);
+            console.log(res.data.data);
             this.data=res.data.data.banner;
             this.List=res.data.data.channel;
+            this.dataList=res.data.data.brandList;
+            this.product=res.data.data.newGoodsList;
+            this.hot=res.data.data.hotGoodsList;
         })
     }
 }
--- app/src/views/Home/Index/index.js
+++ app/src/views/Home/Index/index.js
@@ -1,59 +1,108 @@
-import React, { Component } from 'react';
-import {inject,observer}from "mobx-react";
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
 import "../../../scss/global.scss";
 import Swiper from "swiper/dist/js/swiper.js";
 import "swiper/dist/css/swiper.min.css";
 @inject("home")
 @observer
 class index extends Component {
-    render() {
-        console.log(this.props.home);
-        return (
-            <>
-            <header>
-            <div className='swiper-container'>
-                <div className='swiper-wrapper'>
-                {
-                       this.props.home && this.props.home.data.map(item => (
-                           <div className="swiper-slide" key={item.id}>
-                              <img src={item.image_url} alt="" />
-                             </div>
-                                    ))
-
-                                }
-
-                </div>
-                <div className='swiper-pagination'></div>
+  render() {
+    console.log(this.props.home);
+    return (
+      <>
+        <section>
+          <header>
+            <div className="swiper-container">
+              <div className="swiper-wrapper">
+                {this.props.home &&
+                  this.props.home.data.map(item => (
+                    <div className="swiper-slide" key={item.id}>
+                      <img src={item.image_url} alt="" />
+                    </div>
+                  ))}
+              </div>
+              <div className="swiper-pagination" />
             </div>
-            </header>
-            <section>
-                <div className="list">
-                    {
-                        this.props.home && this.props.home.List.map(item=>(
-                        <span key={item.id}>{item.name}</span>
-                        )) 
-                    }
+          </header>
+
+          <div className="list">
+            {this.props.home &&
+              this.props.home.List.map(item => (
+                <dl key={item.id}>
+                  <dt>
+                    <img src={item.icon_url} alt="" />
+                  </dt>
+                  <dd>{item.name}</dd>
+                </dl>
+              ))}
+          </div>
+          <div className="list_name">
+            <span className="tit">品牌制造商直供</span>
+            <ul>
+              {this.props.home.dataList.map(item => (
+                <li key={item.id}>
+                  <img src={item.new_pic_url} alt="" />
+                  <div>
+                    <span>
+                      {item.name}
+                      <br />
+                      <p>{item.floor_price}元起</p>
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="list_product">
+            <span className="tit">新品首发</span>
+            <div className="dls">
+              {this.props.home.product.map(item => (
+                <dl key={item.id}>
+                  <dt>
+                    <img src={item.list_pic_url} alt="" />
+                  </dt>
+                  <dd>
+                    <span>{item.name}</span>
+                    <span>￥{item.retail_price}</span>
+                  </dd>
+                </dl>
+              ))}
+            </div>
+          </div>
+          <div className="list_hot">
+                <span className="tit">人气推荐</span>
+                <div className="hotGoodsBox">
+                {
+                  this.props.home.hot.map(item=>(
+                    <dl key={item.id}>
+                      <dt>
+                        <img src={item.list_pic_url} alt=""/>
+                      </dt>
+                      <dd>{item.name}</dd>
+                    </dl>
+                  ))
+                }
+
                 </div>
-                            
-            </section>
-            </>
-        );
-    }
-    componentDidMount() {
-        new Swiper('.swiper-container',{
-            loop: true,//这里是自动轮播
-            autoplay: {
-                delay: 2000,
-                disableOnInteraction: false
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                type:"bullets"
-              }
-        });
-        this.props.home.change();
-    }
-    
+          </div>
+        </section>
+      </>
+    );
+  }
+  componentDidMount() {
+    new Swiper(".swiper-container", {
+      loop: true, //这里是自动轮播
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets"
+      }
+    });
+    this.props.home.change();
+  }
 }
 
 export default index;
